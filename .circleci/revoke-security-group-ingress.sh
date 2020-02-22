98846c98fc5ab8545b43f4e296c7b8f10f114310
sendo <abcy.ns.gbd.9st@gmail.com>
2020-02-22 17:55:12 +0900

    modify circleci yaml

---
+++ .circleci/revoke-security-group-ingress.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+IP=`curl -s ifconfig.me`
+echo "#!/bin/bash" > ./sg.sh
+echo "aws configure set region ap-northeast-1" >> ./sg.sh
+echo "aws ec2 revoke-security-group-ingress --group-id sg-0105d7fb2205fa6cf --protocol tcp --port 22 --cidr ${IP}/32" >> ./sg.sh
+bash ./sg.sh
